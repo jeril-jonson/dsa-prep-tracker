a8001c8a564bad59b205ae2b828b1a3abe9c0034
Jeril <jeriljohnson00@gmail.com>
2026-01-15 19:46:38 +0530

    Service class is created

---
+++ DsaTrackerServices/Program.cs
@@ -0,0 +1,38 @@
+﻿using DsaDAL.Models;
+using DsaTrackerDAL;
+
+
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Controllers
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//  Register DbContext (Scoped by default)
+builder.Services.AddDbContext<DsaPrepTrackerDBContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("HealthCareDBConnectionString")
+    )
+);
+
+// Register Repository as TRANSIENT
+builder.Services.AddTransient<DsaTrackerRepository>();
+
+var app = builder.Build();
+
+// Pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
