aaa5eafeb10a39e780c08cd995ff862944788db7
Jeril <jeriljohnson00@gmail.com>
2026-01-15 19:28:51 +0530

    appsettings.json is added

--- DsaDAL/Models/DsaPrepTrackerDBContext.cs
+++ DsaDAL/Models/DsaPrepTrackerDBContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace DsaDAL.Models
 {
@@ -21,10 +22,14 @@ namespace DsaDAL.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            var builder=new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            var config=builder.Build();
+            var connectionString=config.GetConnectionString("DsaPrepTrackerDBConnectionString");    
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source =(localdb)\\MSSQLLocalDB;Initial Catalog=DsaPrepTrackerDB;Integrated Security=true");
+                optionsBuilder.UseSqlServer(connectionString);
             }
         }
 
